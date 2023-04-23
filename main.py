8b2a9614b1744cca8cc62570800cdc24d6ea2d15
ras5555 <130898834+ras5555@users.noreply.github.com>
2023-04-23 14:31:11 +0500

    Create main.py
    version1.0

---
+++ main.py
@@ -0,0 +1,15 @@
+from pygame import *
+window = display.set_mode((700,500))
+display.set_caption("Шутер")
+background = transform.scale(image.load('background.jpg'), (700,500))
+clock = time.Clock()
+
+game = True
+while game:
+    window.blit(background,(0,0))
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    display.update()
+    clock.tick(60)
